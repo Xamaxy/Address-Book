6f4928961bf09b8a43610fbedb8a888329e66d74
Martin Potočnik <martin.potocnik85@gmail.com>
2020-06-22 20:37:53 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ class Contact {
     this.lastName = lastName;
     this.phone = phone;
     this.email = email;
-  }
+    this.id = Contact.incrementId()
+  }  
+
+  static incrementId() {
+    if (!this.latestId) this.latestId = 1;
+    else this.latestId++
+    return this.latestId
+ }
 }
 
 
@@ -13,16 +20,30 @@ class UI {
   addContactToList(contact) {
     const tableList = document.getElementById('contact-list');
     const row = document.createElement('tr');
+    row.id = `${contact.id}`;
+    // row.className = 'list';
     row.innerHTML = `
       <td>${contact.firstName}</td>
       <td>${contact.lastName}</td>
       <td>${contact.phone}</td>
       <td>${contact.email}</td> 
       <td>
-          <a href="#" class="edit card-link"><i class="fa fa-pencil"></i></a>
-          <a href="#" class="delete card-link"><i class="fa fa-remove"></i></a>
+          <a href="#" class="edit card-link" id="${contact.id}"><i class="fa fa-pencil"></i></a>
+          <a href="#" class="delete card-link" id="${contact.id}"><i class="fa fa-remove"></i></a>
       </td>
     `;
+
+    // let ID;
+    // if(contact.length > 0) {
+    //   ID = contact[contact.length -1].id + 1;
+    // } else {
+    //   ID = 0;
+    // }
+
+    // let ID = document.getElementsByClassName('test');
+    // for (let i = 0; i <ID.length; i++) {
+    //   ID[i].id = `${contact.id}` + (i + 1);
+    // }
 
     tableList.appendChild(row);
   }
@@ -32,8 +53,8 @@ class UI {
     div.className = className;
     div.appendChild(document.createTextNode(message));
     const container = document.querySelector('.contactContainer');
-    const banana = document.querySelector('#contact-form');
-    container.insertBefore(div, banana);
+    const form = document.querySelector('#contact-form');
+    container.insertBefore(div, form);
 
     setTimeout(() => {
       document.querySelector('.alert').remove();
@@ -57,21 +78,48 @@ class UI {
 
 class Storage {
   static getContacts() {
+    let contacts;
+    if(localStorage.getItem('contacts') === null) {
+      contacts = [];
+    } else {
+      contacts = JSON.parse(localStorage.getItem('contacts'));
+    }
 
+    return contacts;
   }
 
   static displayContacts() {
+    const contacts = Storage.getContacts();
 
+    contacts.forEach((contact) => {
+      const ui = new UI;
+
+      ui.addContactToList(contact);
+    });
   }
 
-  static addContact() {
+  static addContact(contact) {
+    const contacts = Storage.getContacts();
 
+    contacts.push(contact);
+
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   }
 
-  static removeContact() {
+  static removeContact(contactItem) {
+    console.log(contactItem);
+    const contacts = Storage.getContacts();
 
+    contacts.forEach((contact, index) => {
+      if(contact.contactItem === contactItem) {
+        contacts.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   }
 }
+
 
 
 
@@ -82,14 +130,16 @@ document.getElementById('contact-submit').addEventListener('click', function(e) 
   const lastName = document.getElementById('lastname').value;
   const phone = document.getElementById('phone').value;
   const email = document.getElementById('email').value;
+  // const id = document.getElementById('id').value;
 
   const contact = new Contact(firstName, lastName, phone, email);
   const ui = new UI();
 
-  if (firstName, lastName, phone, email === '') {
+  if (firstName === '' && lastName === '' && phone === '' && email === '') {
     ui.showAlert('Please fill in any field', 'alert alert-danger');
   } else {
     ui.addContactToList(contact);
+    Storage.addContact(contact);
     ui.showAlert('Contact Added!', 'alert alert-success');
     ui.clearFields();
   }
@@ -102,10 +152,18 @@ document.getElementById('contact-submit').addEventListener('click', function(e) 
 // Event listener for delete
 document.getElementById('contact-list').addEventListener('click', function(e) {
   const ui = new UI();
+
   if (e.target.parentElement.classList.contains('delete')) {
-    ui.deleteContact(e.target);
-    ui.showAlert('Contact Deleted!', 'alert alert-success');
+    if(confirm('Are you sure?')) {
+      ui.deleteContact(e.target);
+      Storage.removeContact(e.target.parentElement.parentElement.parentElement.id.textContent);
+      ui.showAlert('Contact Deleted!', 'alert alert-success');      
+    }
   }
   
   e.preventDefault();
 });
+
+
+// DOM load event
+document.addEventListener('DOMContentLoaded', Storage.displayContacts);
