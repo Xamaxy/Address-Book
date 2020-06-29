a47ceac038d9071d73fcb38143c1b11982ba2cac
Martin Potočnik <martin.potocnik85@gmail.com>
2020-06-29 18:24:54 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,14 +4,8 @@ class Contact {
     this.lastName = lastName;
     this.phone = phone;
     this.email = email;
-    this.id = Contact.incrementId()
+    this.id = String(Date.now());
   }  
-
-  static incrementId() {
-    if (!this.latestId) this.latestId = 1;
-    else this.latestId++
-    return this.latestId
- }
 }
 
 
@@ -20,30 +14,17 @@ class UI {
   addContactToList(contact) {
     const tableList = document.getElementById('contact-list');
     const row = document.createElement('tr');
-    row.id = `${contact.id}`;
-    // row.className = 'list';
+    row.className = 'contact-item';
     row.innerHTML = `
       <td>${contact.firstName}</td>
       <td>${contact.lastName}</td>
       <td>${contact.phone}</td>
       <td>${contact.email}</td> 
       <td>
-          <a href="#" class="edit card-link" id="${contact.id}"><i class="fa fa-pencil"></i></a>
-          <a href="#" class="delete card-link" id="${contact.id}"><i class="fa fa-remove"></i></a>
+        <a href="#" class="edit card-link" id="${contact.id}"><i class="fa fa-pencil"></i></a>
+        <a href="#" class="delete card-link" id="${contact.id}"><i class="fa fa-remove"></i></a>
       </td>
     `;
-
-    // let ID;
-    // if(contact.length > 0) {
-    //   ID = contact[contact.length -1].id + 1;
-    // } else {
-    //   ID = 0;
-    // }
-
-    // let ID = document.getElementsByClassName('test');
-    // for (let i = 0; i <ID.length; i++) {
-    //   ID[i].id = `${contact.id}` + (i + 1);
-    // }
 
     tableList.appendChild(row);
   }
@@ -70,6 +51,19 @@ class UI {
     document.getElementById('lastname').value = '';
     document.getElementById('phone').value = '';
     document.getElementById('email').value = '';
+  }
+
+  validateEmail() {
+    const email = document.getElementById('email').value;
+    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+    return re.test(email);
+  
+    // if(!re.test(email.value)){
+    //   email.classList.add('is-invalid');
+    // } else {
+    //   email.classList.remove('is-invalid');
+    // }
   }
 }
 
@@ -106,21 +100,26 @@ class Storage {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }
 
-  static removeContact(contactItem) {
-    console.log(contactItem);
+  static removeContact(id) {    
     const contacts = Storage.getContacts();
 
     contacts.forEach((contact, index) => {
-      if(contact.contactItem === contactItem) {
+      if(contact.id === id) {
         contacts.splice(index, 1);
       }
     });
 
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }
+
+  // Option B for removal
+  // static removeContact(contactItem) {
+  //   const newcontactList = Storage.getContacts().filter( contact => contact.id.toString() !== contactItem.toString())
+
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }
+
 }
-
-
 
 
 
@@ -130,7 +129,6 @@ document.getElementById('contact-submit').addEventListener('click', function(e) 
   const lastName = document.getElementById('lastname').value;
   const phone = document.getElementById('phone').value;
   const email = document.getElementById('email').value;
-  // const id = document.getElementById('id').value;
 
   const contact = new Contact(firstName, lastName, phone, email);
   const ui = new UI();
@@ -138,10 +136,15 @@ document.getElementById('contact-submit').addEventListener('click', function(e) 
   if (firstName === '' && lastName === '' && phone === '' && email === '') {
     ui.showAlert('Please fill in any field', 'alert alert-danger');
   } else {
-    ui.addContactToList(contact);
-    Storage.addContact(contact);
-    ui.showAlert('Contact Added!', 'alert alert-success');
-    ui.clearFields();
+    if(ui.validateEmail(email) === true || email === '') {
+      ui.addContactToList(contact);
+      Storage.addContact(contact);
+      ui.showAlert('Contact Added!', 'alert alert-success');
+      ui.clearFields();      
+    } else { 
+      // if(ui.validateEmail(email) === false)      
+      ui.showAlert('Please enter a valid email format', 'alert alert-danger');
+    }
   }
 
   e.preventDefault();
@@ -149,14 +152,15 @@ document.getElementById('contact-submit').addEventListener('click', function(e) 
 
 
 
+
 // Event listener for delete
 document.getElementById('contact-list').addEventListener('click', function(e) {
   const ui = new UI();
 
-  if (e.target.parentElement.classList.contains('delete')) {
+  if (e.target.parentElement.classList.contains('delete')) {    
     if(confirm('Are you sure?')) {
       ui.deleteContact(e.target);
-      Storage.removeContact(e.target.parentElement.parentElement.parentElement.id.textContent);
+      Storage.removeContact(e.target.parentElement.id);
       ui.showAlert('Contact Deleted!', 'alert alert-success');      
     }
   }
@@ -165,5 +169,56 @@ document.getElementById('contact-list').addEventListener('click', function(e) {
 });
 
 
+
 // DOM load event
 document.addEventListener('DOMContentLoaded', Storage.displayContacts);
+
+
+
+// Event listener for search
+document.getElementById('filter').addEventListener('keyup', filterContacts);
+
+function filterContacts(e) {
+  const text = e.target.value.toLowerCase();
+  const tableList = document.getElementById('contact-list');
+  const tr = tableList.getElementsByTagName('tr');
+
+  for ( i = 0; i < tr.length; i++ ) {
+    // let td, txtValue;
+    td = tr[i].getElementsByTagName('td')[0];
+    txtValue = td.textContent || td.innerText;
+    if (txtValue.toLowerCase().indexOf(text) > -1) {
+      console.log(td);
+      tr[i].style.display = '';
+    } else {
+      tr[i].style.display = 'none';
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+// OVO RADI na firstname zbog [0]
+// function filterContacts(e) {
+//   const text = e.target.value.toLowerCase();
+//   const tableList = document.getElementById('contact-list');
+//   const tr = tableList.getElementsByTagName('tr');
+
+//   for ( i = 0; i < tr.length; i++ ) {
+//     // let td, txtValue;
+//     td = tr[i].getElementsByTagName('td')[0];
+//     txtValue = td.textContent || td.innerText;
+//     if (txtValue.toLowerCase().indexOf(text) > -1) {
+//       console.log(td);
+//       tr[i].style.display = '';
+//     } else {
+//       tr[i].style.display = 'none';
+//     }
+//   }
+// }
