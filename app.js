4bd7994a50d0c92d6e5f8c6e175da30b982e1652
Martin Potočnik <martin.potocnik85@gmail.com>
2020-08-07 16:56:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -8,71 +8,6 @@ class Contact {
   }  
 }
 
-
-
-class UI {
-  addContactToList(contact) {
-    const tableList = document.getElementById('contact-list');
-    const row = document.createElement('tr');
-    row.className = 'contact-item';
-    row.innerHTML = `
-      <td>${contact.firstName}</td>
-      <td>${contact.lastName}</td>
-      <td>${contact.phone}</td>
-      <td>${contact.email}</td> 
-      <td>
-        <a href="#" class="edit card-link" id="${contact.id}"><i class="fa fa-pencil"></i></a>
-        <a href="#" class="delete card-link" id="${contact.id}"><i class="fa fa-remove"></i></a>
-      </td>
-    `;
-
-    tableList.appendChild(row);
-  }
-
-  showAlert(message, className) {
-    const div = document.createElement('div');
-    div.className = className;
-    div.appendChild(document.createTextNode(message));
-    const container = document.querySelector('.contactContainer');
-    const form = document.querySelector('#contact-form');
-    container.insertBefore(div, form);
-
-    setTimeout(() => {
-      document.querySelector('.alert').remove();
-    }, 3000);
-  }
-
-  deleteContact(target) {
-    target.parentElement.parentElement.parentElement.remove();
-  }
-
-  clearFields() {
-    document.getElementById('firstname').value = '';
-    document.getElementById('lastname').value = '';
-    document.getElementById('phone').value = '';
-    document.getElementById('email').value = '';
-  }
-
-  validateEmail() {
-    const email = document.getElementById('email').value;
-    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-
-    return re.test(email);
-  }
-
-  validatePhone(){
-    const phone = document.getElementById('phone').value;
-    const re = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
-
-    return re.test(phone);
-  }
-  
-
-}
-
-
-
-
 class Storage {
   static getContacts() {
     let contacts;
@@ -122,6 +57,214 @@ class Storage {
   //   localStorage.setItem('contacts', JSON.stringify(contacts));
   // }
 
+  static updateContact(updatedContact) {
+    const contacts = Storage.getContacts();
+
+    contacts.forEach((contact, index) => {
+      if(contact.id === updatedContact.id){
+        contacts.splice(index, 1, updatedContact);
+      }
+    });
+
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }
+
+}
+
+
+
+class UI {
+  addContactToList(contact) {
+    const tableList = document.getElementById('contact-list');
+    const row = document.createElement('tr');
+    row.className = 'contact-item';
+    row.innerHTML = `
+      <td>${contact.firstName}</td>
+      <td>${contact.lastName}</td>
+      <td>${contact.phone}</td>
+      <td>${contact.email}</td> 
+      <td>
+        <a href="#" class="edit card-link" id="${contact.id}"><i class="fa fa-pencil"></i></a>
+        <a href="#" class="delete card-link" id="${contact.id}"><i class="fa fa-remove"></i></a>
+      </td>
+    `;
+
+    tableList.appendChild(row);
+  } 
+
+  updateContact(updatedContact) {
+    // tr?
+    const contactItems = document.querySelectorAll('.contact-item');
+    // ?
+    // contactItems = Array.from(contactItems);
+
+    contactItems.forEach(contact => {
+      const itemID = document.getElementById('id').value; 
+      console.log(itemID);
+        if(itemID === updatedContact.id) {
+          console.log(true);
+          
+          // OVO RADI
+          contact.firstName = updatedContact.firstName;
+          contact.lastName = updatedContact.lastName;
+          contact.phone = updatedContact.phone;
+          contact.email = updatedContact.email;
+          contact.id = updatedContact.id;          
+
+
+          // contact.innerHTML = `
+          //   <td>${updatedContact.firstName}</td>
+          //   <td>${updatedContact.lastName}</td>
+          //   <td>${updatedContact.phone}</td>
+          //   <td>${updatedContact.email}</td> 
+          //   <td>
+          //     <a href="#" class="edit card-link" id="${updatedContact.id}"><i class="fa fa-pencil"></i></a>
+          //     <a href="#" class="delete card-link" id="${updatedContact.id}"><i class="fa fa-remove"></i></a>
+          //   </td>
+          // `;
+        }
+    });
+   
+  }
+
+  showContacts() {
+    let output = '';
+    const contacts = Storage.getContacts();
+    contacts.forEach(contact => {
+      output += `
+        <tr class="contact-item">
+          <td>${contact.firstName}</td>
+          <td>${contact.lastName}</td>
+          <td>${contact.phone}</td>
+          <td>${contact.email}</td> 
+          <td>
+            <a href="#" class="edit card-link" id="${contact.id}"><i class="fa fa-pencil"></i></a>
+            <a href="#" class="delete card-link" id="${contact.id}"><i class="fa fa-remove"></i></a>
+          </td>
+        </tr>
+    `;
+    });
+    const tableList = document.getElementById('contact-list');
+    tableList.innerHTML = output;
+  }
+
+  // populateUI() {
+  //   const contacts = Storage.getContacts();
+
+  //   contacts.forEach(contact => {
+  //     const tableList = document.getElementById('contact-list');
+  //     console.log(tableList)
+  //     const row = document.createElement('tr');
+  //     row.className = 'contact-item';
+  //     row.innerHTML = `
+  //       <td>${contact.firstName}</td>
+  //       <td>${contact.lastName}</td>
+  //       <td>${contact.phone}</td>
+  //       <td>${contact.email}</td> 
+  //       <td>
+  //         <a href="#" class="edit card-link" id="${contact.id}"><i class="fa fa-pencil"></i></a>
+  //         <a href="#" class="delete card-link" id="${contact.id}"><i class="fa fa-remove"></i></a>
+  //       </td>
+  //     `;
+  
+  //     tableList.appendChild(row);
+  //   })
+  // }
+  
+  
+  showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = className;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.contactContainer');
+    const form = document.querySelector('#contact-form');
+    container.insertBefore(div, form);
+
+    setTimeout(() => {
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
+
+  deleteContact(target) {
+    target.parentElement.parentElement.parentElement.remove();
+  }
+
+  clearFields() {
+    document.getElementById('firstname').value = '';
+    document.getElementById('lastname').value = '';
+    document.getElementById('phone').value = '';
+    document.getElementById('email').value = '';
+  }
+
+  clearEditState() {
+    document.getElementById('contact-submit').style.display = 'block';
+    document.getElementById('cancel').style.display = 'none';
+    document.getElementById('contact-update').style.display = 'none';
+  }
+
+  validateEmail() {
+    const email = document.getElementById('email').value;
+    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+    return re.test(email);
+  }
+
+  validatePhone(){
+    const phone = document.getElementById('phone').value;
+    const re = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+
+    return re.test(phone);
+  }
+
+  fillForm(data) {
+    document.getElementById('firstname').value = data.firstName;
+    document.getElementById('lastname').value = data.lastName;
+    document.getElementById('phone').value = data.phone;
+    document.getElementById('email').value = data.email;
+    document.getElementById('id').value = data.id;    
+  }
+
+  editState() {
+    document.getElementById('contact-submit').style.display = 'none';
+    document.getElementById('cancel').style.display = 'block';
+    document.getElementById('contact-update').style.display = 'block';
+  }
+  
+  // changeFormState(type) {
+  //   contactSubmit = document.getElementById('contact-submit');
+
+  //   if(type === 'edit') {
+  //     this.contactSubmit.textContent = 'Update Contact';
+  //     this.contactSubmit.className = 'contact-submit btn btn-warning btn-block';
+
+  //     // Create cancel button
+  //     const button = document.createElement('button');
+  //     button.className = 'contact-cancel btn btn-light btn-block';
+  //     button.appendChild(document.createTextNode('Cancel Edit'));
+
+  //     // Get parent
+  //     const contactForm = document.querySelector('.contact-form');
+  //     // Get element to insert before
+  //     const formEnd = document.querySelector('.form-end');
+  //     // Insert cancel button
+  //     contactForm.insertBefore(button, formEnd);
+  //   } else {
+  //     this.contactSubmit.textContent = 'Post It';
+  //     this.contactSubmit.className = 'contact-submit btn btn-primary btn-block';
+  //     // Remove cancel btn if it is there
+  //     if(document.querySelector('.contact-cancel')) {
+  //       document.querySelector('.contact-cancel').remove();
+  //     }
+  //     // Clear ID from hidden field
+  //     this.clearIdInput();
+  //     // Clear text
+  //     this.clearFields();
+  //   }
+  // }
+
+  // clearIdInput() {
+  //   document.getElementById('id').value = '';
+  // }
 }
 
 
@@ -132,8 +275,9 @@ document.getElementById('contact-submit').addEventListener('click', function(e) 
   const lastName = document.getElementById('lastname').value;
   const phone = document.getElementById('phone').value;
   const email = document.getElementById('email').value;
+  const id = document.getElementById('id').value;
 
-  const contact = new Contact(firstName, lastName, phone, email);
+  const contact = new Contact(firstName, lastName, phone, email, id);
   const ui = new UI();
 
   if (firstName === '' && lastName === '' && phone === '' && email === '') {
@@ -156,8 +300,9 @@ document.getElementById('contact-submit').addEventListener('click', function(e) 
 
 
 
+
 // Event listener for delete
-document.getElementById('contact-list').addEventListener('click', function(e) {
+document.getElementById('contact-list').addEventListener('click',  e => {
   const ui = new UI();
 
   if (e.target.parentElement.classList.contains('delete')) {    
@@ -173,19 +318,12 @@ document.getElementById('contact-list').addEventListener('click', function(e) {
 
 
 
-// DOM load event
-document.addEventListener('DOMContentLoaded', Storage.displayContacts);
-
-
-
-// Event listener for search
-document.getElementById('filter').addEventListener('keyup', filterContacts);
-
-function filterContacts(e) {
+// Event listener for search/filter
+document.getElementById('filter').addEventListener('keyup',  e => {
   const text = e.target.value.toLowerCase();
-  const contactItems = document.querySelectorAll('#contact-list tr');
+  let contactItems = document.querySelectorAll('#contact-list tr');
 
-  // contactItems = Array.from(contactItems);
+  contactItems = Array.from(contactItems);
 
   contactItems.forEach(function(contact) {
     const item = contact.textContent;
@@ -194,8 +332,8 @@ function filterContacts(e) {
     } else {
       contact.style.display = 'none';
     }
-  });
-}
+  })
+});
 
 
 
@@ -219,3 +357,106 @@ function filterContacts(e) {
 //     }
 //   }
 // }
+
+
+// Edit state
+document.getElementById('contact-list').addEventListener('click', e => {
+  const ui = new UI();
+
+  if(e.target.parentElement.classList.contains('edit')) {
+    const firstName = e.target.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+    const lastName = e.target.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+    const phone = e.target.parentElement.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const email = e.target.parentElement.parentElement.previousElementSibling.textContent;
+    const id = e.target.parentElement.id;
+    
+    const data = {
+      firstName,
+      lastName,
+      phone,
+      email,
+      id
+    }
+
+    ui.fillForm(data);
+    ui.editState();
+    console.log(data.id);
+  }
+
+  e.preventDefault();
+});
+
+// Cancel button
+document.getElementById('cancel').addEventListener('click', e => {
+  const ui = new UI();
+  ui.clearFields();
+  ui.clearEditState();
+
+  e.preventDefault();
+});
+
+
+// Update button
+document.getElementById('contact-update').addEventListener('click', e => {
+  const ui = new UI();
+
+  const firstName = document.getElementById('firstname').value;
+  const lastName = document.getElementById('lastname').value;
+  const phone = document.getElementById('phone').value;
+  const email = document.getElementById('email').value;
+  const id = document.getElementById('id').value;
+
+  const updatedContact = {
+    firstName,
+    lastName,
+    phone,
+    email,
+    id
+  }
+
+
+  if (firstName === '' && lastName === '' && phone === '' && email === '') {
+    ui.showAlert('Please fill in any field', 'alert alert-danger');
+  } else {
+      if ((ui.validateEmail(email) === true || email === '') && (ui.validatePhone(phone) === true || phone === '')) {
+        ui.updateContact(updatedContact);    
+        Storage.updateContact(updatedContact);
+        ui.showContacts();
+        ui.showAlert('Contact updated!', 'alert alert-success');
+        ui.clearFields();
+        ui.clearEditState(); 
+      } else {
+        ui.showAlert('Please enter a valid email or phone format', 'alert alert-danger');
+      }      
+    }
+
+  e.preventDefault();
+});
+
+
+// Sort contacts
+document.getElementById('sort').addEventListener('click', e => {
+  const ui = new UI();
+
+  const contacts = Storage.getContacts();
+  console.log(contacts)
+  
+  contacts.sort((a, b) => {
+    const firstNameA = a[firstName].toLowerCase();
+    const firstNameB = b[firstName].toLowerCase();
+    if(firstNameA < firstNameB) {
+      return -1;
+    } 
+    if(firstNameA > firstNameB) {
+      return 1;
+    }
+    return 0;
+  });
+   
+  ui.showContacts();
+
+  e.preventDefault();
+});
+
+// DOM load event
+document.addEventListener('DOMContentLoaded', Storage.displayContacts);
